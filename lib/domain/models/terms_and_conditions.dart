e3f6720a0facfdec2300ba942c7ade122c845b89
punesh12-rapidInnovation <punesh@rapidinnovation.dev>
2025-04-29 12:47:17 +0530

    feat/ terms page integration

---
+++ lib/domain/models/terms_and_conditions.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:html_unescape/html_unescape.dart';
+
+class TermsAndConditionsSection {
+  final String title;
+  final String content;
+  final String contentType;
+
+  TermsAndConditionsSection({
+    required this.title,
+    required this.content,
+    required this.contentType,
+  });
+
+  factory TermsAndConditionsSection.fromJson(Map<String, dynamic> json) {
+    final unescape = HtmlUnescape();
+    return TermsAndConditionsSection(
+      title: json['title'] as String? ?? '',
+      content: unescape.convert(json['content'] as String? ?? ''),
+      contentType: json['content_type'] as String? ?? 'TC',
+    );
+  }
+}
+
+class TermsAndConditions {
+  final List<TermsAndConditionsSection> sections;
+  final String lastUpdated;
+  final String content;
+
+  TermsAndConditions({
+    required this.sections,
+    required this.lastUpdated,
+    required this.content,
+  });
+
+  factory TermsAndConditions.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> data = json['data'] as List? ?? [];
+    final unescape = HtmlUnescape();
+    return TermsAndConditions(
+      sections: data
+          .map((section) => TermsAndConditionsSection.fromJson(section))
+          .toList(),
+      lastUpdated: json['updated_at'] as String? ?? DateTime.now().toString(),
+      content: unescape.convert(json['content'] as String? ?? ''),
+    );
+  }
+}
